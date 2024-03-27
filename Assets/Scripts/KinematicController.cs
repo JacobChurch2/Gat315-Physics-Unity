584383cd948f0e72d19c4d141a8f12163bd0045c
JacobChurch2 <jchurch@student.neumont.edu>
2024-03-27 11:56:04 -0600

    Collision/RigidBody

--- Assets/Scripts/KinematicController.cs
+++ Assets/Scripts/KinematicController.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class KinematicController : MonoBehaviour
 {
     [SerializeField] float speed = 1;
+    [SerializeField] Space space = Space.World;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 direction = Vector3.zero;
 
-        direction.x = Input.GetAxis("Horizontal");
+        float rotation = 0;
+
+        if(space == Space.World) direction.x = Input.GetAxis("Horizontal");
+        else
+        {
+            rotation = Input.GetAxis("Horizontal");
+        }
         direction.z = Input.GetAxis("Vertical");
+        direction = Vector3.ClampMagnitude(direction, 1);
 
-        Vector3.ClampMagnitude(direction, 1);
+        transform.rotation *= Quaternion.Euler(0, rotation * speed, 0);
+        //direction = transform.rotation * direction;
 
-        transform.position += direction * speed * Time.deltaTime;
+        transform.Translate(direction * speed * Time.deltaTime, space);
+
+        //transform.position += direction * speed * Time.deltaTime;
     }
 }
