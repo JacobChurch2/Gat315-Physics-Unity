9b358439905a4a92def2806411b2b7c58f2560ef
JacobChurch2 <jchurch@student.neumont.edu>
2024-03-31 12:33:00 -0600

    ragdoll/vehicle

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject Item;
+    [SerializeField] KeyCode keyCode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            Instantiate<GameObject>(Item, this.transform);
+        }
+    }
+}
